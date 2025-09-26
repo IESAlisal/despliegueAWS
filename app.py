2ce0255f59f7581e8caaedb1941d0fd84dc99fb0
Javier Terán González <javier.teran@educantabria.es>
2025-09-26 19:06:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola desde la app Python en ECS Fargate!. IES Alisal4."
+    return "¡Hola desde la app Python en ECS Fargate!. IES Alisal."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
